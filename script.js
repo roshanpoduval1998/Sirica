2d562f404afc79d378db9f966e0ba0bba5d2e1d4
Roshan <46379406+roshanpoduval1998@users.noreply.github.com>
2021-09-19 20:55:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("official-Header-title");
+var headersup = document.getElementById("sup_");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+	header.style.fontSize = "5vw";
+	headersup.style.fontSize = "2vw";
+  } else {
+    header.classList.remove("sticky");
+	header.style.fontSize = "9vw";
+	headersup.style.fontSize = "3vw";
+  }
+}
